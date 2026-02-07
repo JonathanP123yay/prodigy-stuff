fd9fb52bb184b49b13eb97bee42e8ed38556cd14
JonathanP123yay <165610530+JonathanP123yay@users.noreply.github.com>
2026-02-06 21:55:02 -0500

    Remove convertMain function and bookmarklet logic
    Removed the convertMain function and its associated logic for generating a bookmarklet from main.js.

--- Convert.js
+++ Convert.js
@@ -23,28 +23,3 @@ async function convertMain() {
     console.error(err);
   }
 }
-async function convertMain() {
-  try {
-    const res = await fetch("./main.js");
-    let code = await res.text();
-
-    // Remove comments
-    code = code.replace(/\/\*[\s\S]*?\*\//g, "");
-    code = code.replace(/\/\/.*$/gm, "");
-
-    // Compress whitespace
-    code = code.replace(/\s+/g, " ").trim();
-
-    // Wrap as bookmarklet
-    const bookmarklet = "javascript:(()=>{" + code + "})()";
-
-    // Output to page
-    const box = document.getElementById("output");
-    box.value = bookmarklet;
-
-    console.log("Bookmarklet generated!");
-  } catch (err) {
-    alert("Failed to load main.js");
-    console.error(err);
-  }
-}
